ca4f18844616f3e893cb6af67b1216cb3ae7b446
Eduardo Pacheco <eduardo@eapprojetos.com.br>
2025-04-22 19:53:34 -0300

    feat: 1003 - Soma Simples - accepted

---
+++ iniciante/nivel1/somasimples1003/Main.java
@@ -0,0 +1,14 @@
+package iniciante.nivel1.somasimples1003;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        System.out.println("SOMA = " + (a + b));
+    }
+}
